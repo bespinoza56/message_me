cb73742bd7d5db0ffeec51f1ec393e4d2f8361a8
Bradley Espinoza <bespinoza@doximity.com>
2020-05-04 23:53:27 -0700

    Wired up messages to display

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "john", password: "password")
+User.create(username: "maribel", password: "password")
+User.create(username: "arnold", password: "password")
+User.create(username: "sam", password: "password")
+User.create(username: "frank", password: "password")
